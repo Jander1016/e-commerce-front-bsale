c096d3661ecf95ba4c29c0921baf6aeaeabd1169
Jander Gomez <jandergb.30@gmail.com>
2022-11-13 15:54:56 -0500

    feaat: modularize project

--- src/app.js
+++ src/app.js
@@ -1,120 +1,32 @@
+//Importaciones
+import { LoadInitialproducts, LoadCategory } from './helpers/intialData.js';
+import { findCategoryProduct, findNameProduct } from './helpers/findProducts.js';
+import addCart, { deleteCart, deleteAllCart } from './components/cart/Cart.js';
+
 // variables
 let nameProduct = document.querySelector('#name-product');
 let findProduct = document.querySelector('#findName');
 let categoryProductList = document.querySelector('#category-product')
+let detailProduct = document.querySelector('#app')
+let btnDelete = document.querySelector('.modal-list')
+let btnAllDelete = document.querySelector('#btn-all-delete')
+let btnBuy = document.querySelector('#btn-buy')
+let darkMode = document.querySelector('#dark-mode')
 
+darkMode.addEventListener('click', () => document.documentElement.classList.toggle('dark-mode'));
 
+btnAllDelete.addEventListener('click', deleteAllCart)
 
-function DisplayProducts(products) {
-  try {
-    document.getElementById('app').innerHTML = ""
-    let productHTML = ''
-    products.forEach(element => {
-      productHTML += `<section class="col d-flex justify-content-center flex-row">
-              <div class="card my-3 py-2 shadow rounded" style="width: 15rem;">
-                <figure class="figure">
-                    <img class ="img-product" loading='lazy' style="width: 100%" src=${
-        element.url_image || './images/products/' + element.id + '.jpg'
-      } alt=${
-        element.name
-      }>
-                </figure>
-                <div class="card-title text-center my-1 py-1">
-                    <span class="card-title">${
-        element.name
-      }</span>
-                </div>
-                <div class="card-body">
-                    <span class="price">$${
-        element.price
-      }</span>
-                    <button id="btn-add-cart" class="btn btn-secondary fa fa-cart-arrow-down"></button>
-                </div>
-              </div>
-            </section>`
-    });
-    document.getElementById('app').innerHTML = productHTML;
+btnBuy.addEventListener('click', deleteAllCart)
 
-  } catch (error) {
-    document.getElementById('app').innerHTML = `<section class="col d-flex justify-content-center flex-row">
-        <div class="card my-3 py-2 shadow rounded" style="width: 20rem;">
-          <div class="card-image">
-            <img src="./images/NotFound.jpg" alt="notfound" >
-          </div>
-        </div>
-      </section>`
-  }
-}
+btnDelete?.addEventListener('click', deleteCart)
 
+detailProduct.addEventListener('click', addCart)
 
-async function loadCategory() {
-  try {
-    const categoryList = await(await fetch('https://e-commerce-back-bsale-production.up.railway.app/api/v1/categories')).json()
-    if (categoryList.status !== 'OK') 
-      return
-    
-    let dataCategory = '<option selected>Select Category</option> '
-    categoryList.data.map((category) => {
-      dataCategory += `<option value='${category.name}' >${category.name}</option>`
-    })
-    document.getElementById('category-product').innerHTML = dataCategory
-  } catch (error) {
-    console.log(error.message)
-  }
-}
-
-
-window.onload = async function () {
-  try {
-    const productsList = await(await fetch('https://e-commerce-back-bsale-production.up.railway.app/api/v1/products/')).json()
-    await loadCategory()
-    if (productsList.status !== 'OK') 
-      return
-    
-    const dataProducts = productsList.data
-    DisplayProducts(dataProducts)
-  } catch (error) {
-    console.log(error.message)
-  }
-}
-
-
-async function findNameProduct(param) {
-  try {
-    categoryProductList.selectedIndex = 0
-    const productsList = await(await fetch('https://e-commerce-back-bsale-production.up.railway.app/api/v1/products/' + param)).json()
-
-    if (productsList.data.length === 0) {
-      const notFound = `<section class="col d-flex justify-content-center flex-row">
-              <img src='./images/NotFound.jpg' alt="notfound style="width: 100%; "" >
-          </section>`
-      console.log(notFound)
-      document.getElementById('app').innerHTML = notFound
-    } else {
-      DisplayProducts(productsList.data)
-    }
-
-  } catch (error) {
-    console.log(error.message)
-
-  }
-}
-
-async function findCategoryProduct(param) {
-  try {
-    if (! param) 
-      return
-    
-    const categoryProduct = await(await fetch(`https://e-commerce-back-bsale-production.up.railway.app/api/v1/products/category/${param}`)).json()
-    if (categoryProduct.data.length === 0) {
-      const allData= await(await fetch('https://e-commerce-back-bsale-production.up.railway.app/api/v1/products/')).json()
-      DisplayProducts(allData.data)
-    } else {
-      DisplayProducts(categoryProduct.data)
-    }
-  } catch (error) {
-    console.log(error.message)
-  }
+window.onload =   function () {
+  Promise.allSettled([LoadCategory(), LoadInitialproducts()])
+  .then(values=> values)
+  .catch(error=> console.log(error.message))
 }
 
 findProduct.addEventListener('click', (e) => e.preventDefault());
