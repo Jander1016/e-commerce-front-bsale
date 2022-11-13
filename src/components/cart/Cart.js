cfafc11b44f0299ccda99eb55b79a8b4605725a4
Jander Gomez <jandergb.30@gmail.com>
2022-11-13 17:52:07 -0500

    feat: add data persistence to Cart with localStorage

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,70 +1,72 @@
-
 let cartProducts = []
 let totalCart = 0
 let amountProduct = 0
 
 
-export default function addCart(e){
+export default function addCart(e) {
   e.preventDefault()
   try {
+    if (localStorage.length) cartProducts = JSON.parse(localStorage.getItem('cart'))
+    
     if (e.target.classList.value === "btn btn-secondary fa fa-cart-arrow-down") {
       let name = e.target.parentElement.parentElement.querySelector('.img-product').alt
       let img = e.target.parentElement.parentElement.querySelector('.img-product').src
       let price = e.target.parentElement.querySelector(".price").textContent
-      let amount = cartProducts.amount || 0
-      
-      const product={
+      let amount = 0
+
+      const product = {
         name,
         img,
         price,
         amount
       }
-  
-      if(cartProducts.filter(prod=> prod.name === product.name).length === 0) {
+
+      if (cartProducts.filter(prod => prod.name === product.name).length === 0) {
         product.amount = 1
         cartProducts.push(product)
-        localStorage.setItem('cart',JSON.stringify(cartProducts))
-        totalCart+= parseFloat(price)
+        localStorage.setItem('cart', JSON.stringify(cartProducts))
+        totalCart += parseFloat(price)
         amountProduct++
-      }else{
-        cartProducts.filter(prod=> {
-          prod.name === product.name
-          ? prod.amount++
-          : prod.amount
+      } else {
+        cartProducts.filter(prod => {
+          prod.name === product.name ? prod.amount ++ : prod.amount
         })
-        totalCart+= parseFloat(price)
+        totalCart += parseFloat(price)
         amountProduct++
-        localStorage.setItem('cart',JSON.stringify(cartProducts))
-      }    
-      displayCart(cartProducts)
-      document.querySelector('.total-cart').innerHTML=totalCart
-      document.querySelector('#count-product').textContent= amountProduct
-    }       
-    
+        localStorage.setItem('cart', JSON.stringify(cartProducts))
+      } displayCart(cartProducts)
+      document.querySelector('.total-cart').innerHTML = totalCart
+      document.querySelector('#count-product').textContent = amountProduct
+    }
+
   } catch (error) {
     console.log(error.message)
   }
 }
 
- export function deleteCart(e){
+export function deleteCart(e) {
   e.preventDefault()
   try {
-      if (e.target.classList.value === "btn btn-danger fa-regular fa-trash-can"){
+    if (localStorage.length) cartProducts = JSON.parse(localStorage.getItem('cart'))
+
+    if (e.target.classList.value === "btn btn-danger fa-regular fa-trash-can") {
       const priceDelete = e.target.parentElement.parentElement.querySelector('.priceProductCart').textContent;
       const amountDelete = e.target.parentElement.parentElement.querySelector('.amountProductCart').textContent;
       const nameProductDelete = e.target.parentElement.parentElement.querySelector('.nameProductCart').textContent;
 
-      cartProducts.filter(prod=> {
-        if(prod.name === nameProductDelete) amountProduct = amountProduct - parseInt(amountDelete)
-        document.querySelector('#count-product').textContent= amountProduct
+      cartProducts.filter(prod => {
+        if (prod.name === nameProductDelete) 
+          amountProduct = amountProduct - parseInt(amountDelete)
+        
+        document.querySelector('#count-product').textContent = amountProduct
       })
-      cartProducts = cartProducts.filter(prod=>prod.name !== nameProductDelete )
+      cartProducts = cartProducts.filter(prod => prod.name !== nameProductDelete)
       totalCart = totalCart - (parseFloat(priceDelete) * parseFloat(amountDelete))
 
-      document.querySelector('.total-cart').innerHTML=totalCart;
+      document.querySelector('.total-cart').innerHTML = totalCart;
 
-      localStorage.setItem('cart',JSON.stringify(cartProducts))
-      
+      localStorage.setItem('cart', JSON.stringify(cartProducts))
+
       e.target.parentElement.parentElement.remove()
     }
   } catch (error) {
@@ -72,22 +74,41 @@ export default function addCart(e){
   }
 }
 
-export function deleteAllCart(e){
+export function deleteAllCart(e) {
   e.preventDefault()
   try {
+    if (localStorage.length) cartProducts = JSON.parse(localStorage.getItem('cart'))
     document.querySelector('.modal-list').replaceChildren('')
-      cartProducts = []
-      totalCart = 0
-      amountProduct = 0
-      document.querySelector('.total-cart').innerHTML = totalCart;
-      document.querySelector('#count-product').textContent= amountProduct
-      localStorage.clear()
+    cartProducts = []
+    totalCart = 0
+    amountProduct = 0
+    document.querySelector('.total-cart').innerHTML = totalCart;
+    document.querySelector('#count-product').textContent = amountProduct
+    localStorage.clear()
   } catch (error) {
     console.log(error.message)
   }
 }
 
-function displayCart(products) {
+export function loadCartLocal() {
+  try {
+    if (localStorage.length) {
+      const data = JSON.parse(localStorage.getItem('cart'))
+      displayCart(data)
+      data.map(item => {
+        totalCart += (item.price * item.amount)
+        amountProduct += item.amount
+      })
+      document.querySelector('.total-cart').innerHTML = totalCart
+      document.querySelector('#count-product').textContent = amountProduct
+    }
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+
+export function displayCart(products) {
   try {
     document.querySelector('.modal-list').innerHTML = ""
     let productHTML = ''
